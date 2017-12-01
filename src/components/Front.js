dcf848efcc11ee34ce74c2a236318e7c2fd81426
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-12-01 11:36:15 -0800

    index color

--- src/components/Front.js
+++ src/components/Front.js
@@ -7,7 +7,7 @@ const Front = (props) => {
       <img id="question2" src="https://vignette4.wikia.nocookie.net/community-sitcom/images/a/af/Question_mark.png/revision/latest?cb=20120714155934" alt="Question mark"></img>
     <h1 className="title"> Welcome to Question This! </h1>
     <p id="body">This is an application for people to ask for any potential questions you have about life.
-      <br /><strong>So what are you wating for? Sign in and ask away!</strong></p>
+      <br /><br /><strong>So what are you wating for? Sign in and ask away!</strong></p>
     </div>
   )
 }

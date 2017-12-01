b104b966d3505662078f4a46c182828e21cec95a
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-12-01 11:51:50 -0800

    bulma fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import auth from './auth'
-import 'bulma/css/bulma.css'
+// import 'bulma/css/bulma.css'
 import NavBar from './components/NavBar'
 import User from './components/User'
 import Front from './components/Front'
